7371117b52dca44356f1f1272c757fb86b24e1a0
AlYusril <yusrilefendi12345@gmail.com>
2023-09-10 20:30:40 +0700

    v3 al

--- process-object/index.js
+++ process-object/index.js
@@ -3,7 +3,7 @@ const yourName = process.argv[2];
 const environment = process.env.NODE_ENV;
 
 for(let i=0; i<=10000; i++) {
-
+// coba perulangan
 }
 
 const currentMemoryUsage = process.memoryUsage();
