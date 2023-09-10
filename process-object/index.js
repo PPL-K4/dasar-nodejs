2eaefcc8514bdd71a6d532fae5a2052e68c6c248
AlYusril <yusrilefendi12345@gmail.com>
2023-09-10 20:20:48 +0700

    v2 al

---
+++ process-object/index.js
@@ -0,0 +1,13 @@
+const initialMemoryUsage = process.memoryUsage();
+const yourName = process.argv[2];
+const environment = process.env.NODE_ENV;
+
+for(let i=0; i<=10000; i++) {
+
+}
+
+const currentMemoryUsage = process.memoryUsage();
+
+console.log(`Hai, ${yourName}`);
+console.log(`Mode Environtment: ${initialMemoryUsage}`);
+console.log(`Pengguna memory dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
