b0119b8f5becc88c07988f0c0a89d70c252fb6be
fakehthok <fakehpancen99@gmial.com>
2023-09-10 22:40:32 +0700

    f_filesystem_update

--- filesystem/index.js
+++ filesystem/index.js
@@ -10,11 +10,6 @@ const fileReadCallback = (error,data) => {
 	console.log(data);
 
 };
-// path.resolve (__dirname, 'notes.txt');
+
 fs.readFile('notes.txt', 'UTF-8', fileReadCallback);
-
-
-// const data = fs.readFileSync('./notes.txt', 'UTF-8');
-
-// console.log(data);
 
